15ea06d2d9ec9a08c7534e20c9d28a395e3c58d3
Beatriz Albiero <beatrizalbiero1@gmail.com>
2017-10-17 15:18:14 -0200

    started competion

--- decoding_function.py
+++ decoding_function.py
@@ -244,73 +244,73 @@ def checkcandidates_end(vector2string):
 ##############################################################################################################################################
 import random
 ################
-'''
-checkcandidate
-given a set of wickelfeatures and a position(integer) in the trigram, returns a phoneme candidate
-'''
-def checkcandidate(vector2string,position):
-
-    '''
-    REMINDER:
-    THIS FUNCTION MUST RECEIVE A SET OF WICKELFEATURES REGARDING A SINGLE TRIGRAM.
-    '''
-
-    candidates = MyList('b','p','d','t','g','k','m','n','v','f','z','s','j','x','l','r','a','e','E','i', 'o', 'O', 'u')
-
-    ints = MyList('b','p','d','t','g','k','m','n')
-    conts = MyList('v','f','z','s','j','x','l','r')
-    vowels = MyList('a','e','E','i', 'o', 'O', 'u')
-    b1s = MyList('b','p','d','t','g','k','v','f','z','s','j','x','e','i','o','u')
-    b2s = MyList('m','n','l','r','a','E','O')
-    fronts = MyList('b','p','v','f','m','l','E','i','e')
-    middles = MyList('d','t','n','z','s','r','a')
-    backs = MyList('g','k','j','x','o', 'O', 'u')
-    d1s = MyList('b','d','g','m','v','z','j','l','o','n','r','e')
-    d2s = MyList('p', 't', 'k', 'f', 's', 'x', 'a', 'E', 'O', 'u','i')
-
-    for item in vector2string:
-        if item[position] == '#':
-            pass
-        else:
-            if item[position] == 'int':
-                #print(candidates)
-                candidates = candidates - conts - vowels
-            if item[position] == 'cont':
-                candidates = candidates - ints - vowels
-            if item[position] == 'vowel':
-                candidates = candidates - conts - ints
-                #print(type(candidates))
-            if item[position] == 'b1':
-                #print(candidates)
-                candidates = candidates - b2s
-            if item[position] == 'b2':
-                #print(candidates)
-                candidates = candidates - b1s
-            if item[position] == 'front':
-                #print(candidates)
-                candidates = candidates - middles - backs
-            if item[position] == 'middle':
-                #print(candidates)
-                candidates = candidates - fronts - backs
-            if item[position] == 'back':
-                #print(candidates)
-                candidates = candidates - fronts - middles
-            if item[position] == 'd1':
-                #print(candidates)
-                candidates = candidates - d2s
-            if item[position] == 'd2':
-                #print(candidates)
-                candidates = candidates - d1s
-    return candidates[0]
-
-'''
-Usage:
-'''
-da = cf.coding('#da') #first, string into vector
-Da = cf.vector2string(da,wickelfeatures_list)
-Da #then, vector back to string
-type(checkcandidate(Da,0))
-
+# '''
+# checkcandidate
+# given a set of wickelfeatures and a position(integer) in the trigram, returns a phoneme candidate
+# '''
+# def checkcandidate(vector2string,position):
+#
+#     '''
+#     REMINDER:
+#     THIS FUNCTION MUST RECEIVE A SET OF WICKELFEATURES REGARDING A SINGLE TRIGRAM.
+#     '''
+#
+#     candidates = MyList('b','p','d','t','g','k','m','n','v','f','z','s','j','x','l','r','a','e','E','i', 'o', 'O', 'u')
+#
+#     ints = MyList('b','p','d','t','g','k','m','n')
+#     conts = MyList('v','f','z','s','j','x','l','r')
+#     vowels = MyList('a','e','E','i', 'o', 'O', 'u')
+#     b1s = MyList('b','p','d','t','g','k','v','f','z','s','j','x','e','i','o','u')
+#     b2s = MyList('m','n','l','r','a','E','O')
+#     fronts = MyList('b','p','v','f','m','l','E','i','e')
+#     middles = MyList('d','t','n','z','s','r','a')
+#     backs = MyList('g','k','j','x','o', 'O', 'u')
+#     d1s = MyList('b','d','g','m','v','z','j','l','o','n','r','e')
+#     d2s = MyList('p', 't', 'k', 'f', 's', 'x', 'a', 'E', 'O', 'u','i')
+#
+#     for item in vector2string:
+#         if item[position] == '#':
+#             pass
+#         else:
+#             if item[position] == 'int':
+#                 #print(candidates)
+#                 candidates = candidates - conts - vowels
+#             if item[position] == 'cont':
+#                 candidates = candidates - ints - vowels
+#             if item[position] == 'vowel':
+#                 candidates = candidates - conts - ints
+#                 #print(type(candidates))
+#             if item[position] == 'b1':
+#                 #print(candidates)
+#                 candidates = candidates - b2s
+#             if item[position] == 'b2':
+#                 #print(candidates)
+#                 candidates = candidates - b1s
+#             if item[position] == 'front':
+#                 #print(candidates)
+#                 candidates = candidates - middles - backs
+#             if item[position] == 'middle':
+#                 #print(candidates)
+#                 candidates = candidates - fronts - backs
+#             if item[position] == 'back':
+#                 #print(candidates)
+#                 candidates = candidates - fronts - middles
+#             if item[position] == 'd1':
+#                 #print(candidates)
+#                 candidates = candidates - d2s
+#             if item[position] == 'd2':
+#                 #print(candidates)
+#                 candidates = candidates - d1s
+#     return candidates[0]
+#
+# '''
+# Usage:
+# '''
+# da = cf.coding('#da') #first, string into vector
+# Da = cf.vector2string(da,wickelfeatures_list)
+# Da #then, vector back to string
+# type(checkcandidate(Da,0))
+#
 
 ##############################################################################################################################################
 
@@ -328,7 +328,88 @@ def find_compatible(list1,list2): #returns a list if compatible lists in list2 a
                 compatible.append(item2)
 
     return compatible
-##############################################################################################################################################################
+##############################################################################################################################################
+
+'''
+competion of phonemes
+'''
+def competion(vector2string,position):
+    competitors = {"b":0,"p":0,"d":0,'t':0,'g':0,'k':0,'m':0,'n':0,'v':0,'f':0,'z':0,'s':0,'j':0,'x':0,'l':0,
+                    'r':0,'a':0,'e':0,'E':0,'i':0, 'o':0, 'O':0, 'u':0, '#':0}
+
+    for item in vector2string:
+            if item[position] == '#':
+                competitors['#'] =+1
+
+            if item[position] == 'int':
+                competitors['b'] =+1
+                competitors['p'] =+1
+                competitors['d'] =+1
+                competitors['t'] =+1
+                competitors['g'] =+1
+                competitors['k'] =+1
+                competitors['m'] =+1
+                competitors['n'] =+1
+
+            if item[position] == 'cont':
+                competitors['v'] =+1
+                competitors['f'] =+1
+                competitors['z'] =+1
+                competitors['s'] =+1
+                competitors['j'] =+1
+                competitors['x'] =+1
+                competitors['l'] =+1
+                competitors['r'] =+1
+
+            if item[position] == 'vowel':
+                competitors['a'] =+1
+                competitors['e'] =+1
+                competitors['E'] =+1
+                competitors['i'] =+1
+                competitors['O'] =+1
+                competitors['u'] =+1
+                competitors['o'] =+1
+
+            if item[position] == 'b1':
+                competitors['b'] =+1
+                competitors['p'] =+1
+                competitors['d'] =+1
+                competitors['t'] =+1
+                competitors['g'] =+1
+                competitors['k'] =+1
+                competitors['v'] =+1
+                competitors['f'] =+1
+                competitors['z'] =+1
+                competitors['s'] =+1
+                competitors['j'] =+1
+                competitors['x'] =+1
+                competitors['e'] =+1
+                competitors['i'] =+1
+                competitors['u'] =+1
+                competitors['o'] =+1
+
+            if item[position] == 'b2':
+                competitors['m'] =+1
+                competitors['n'] =+1
+                                
+            if item[position] == 'front':
+                #print(candidates)
+                candidates = candidates - middles - backs
+            if item[position] == 'middle':
+                #print(candidates)
+                candidates = candidates - fronts - backs
+            if item[position] == 'back':
+                #print(candidates)
+                candidates = candidates - fronts - middles
+            if item[position] == 'd1':
+                #print(candidates)
+                candidates = candidates - d2s
+            if item[position] == 'd2':
+                #print(candidates)
+                candidates = candidates - d1s
+
+    return winner
+
 '''
 decoding:
 given a list of wickelfeatures (str), decodes it back into phonemes
