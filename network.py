fee9168d9c08e1523b9c212387de8b0e6a1afdc1
Beatriz Albiero <beatrizalbiero1@gmail.com>
2017-10-30 19:18:46 -0200

    def get batch

--- network.py
+++ network.py
@@ -5,6 +5,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
+import random
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
@@ -12,19 +13,53 @@ numpy.random.seed(7)
 1.Load data
 '''
 
-# load ptverbs dataset
-dataset = numpy.loadtxt("ptverbs.csv", delimiter=",")
+import coding_function as cf
+import csv
+
+'''
+open a csv file containing phonetic transcribed verbs, inifitive and conjugated forms
+'''
+with open('ptverbs_test.csv') as csvfile:
+    readcsv = csv.reader(csvfile, delimiter = ',')
+    phoneticinf = []
+    phoneticI = []
+
+    for row in readcsv:
+        phinf = row[1]
+        phI = row[3]
+        phoneticinf.append(phinf)
+        phoneticI.append(phI)
+
+
+'''
+create two dictionaries, one for inifitive forms and another for conjugated forms
+'''
+
+dictioinf = {}
+dictioI = {}
+
+for item in phoneticinf:
+    dictioinf[item] = cf.coding(item)
+
+for item in phoneticI:
+    dictioI[item] = cf.coding(item)
+
+# dictioinf.values()
+# dictioI
+
+
 # split into input (X) and output (Y) variables
-X = dataset[:,] #input column
-Y = dataset[:,] #output column
+X = dictioinf.values() #input column
+Y = dictioI.values() #output column
+
 
 '''
 2. Define model
 '''
 # create model
 model = Sequential()
-model.add(Dense(460, input_dim=1, activation='relu'))
-model.add(Dense(?, activation='relu'))
+model.add(Dense(460, input_dim=460, activation='relu'))
+#model.add(Dense(?, activation='relu'))
 model.add(Dense(460, activation='sigmoid'))
 
 '''
@@ -37,6 +72,12 @@ model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 '''
 4. Fit model
 '''
+
+def get_batch(n):
+    chosen = random.sample(range(1,20), n)
+    for i in range(n):
+        #todo: how to select dictionary entries?
+    return x, y
 
 model.fit(X, Y, epochs=150, batch_size=10)
 
