0109f30ea55e50d296bb246e44afaa47d9684c40
Beatriz Albiero <beatrizalbiero1@gmail.com>
2017-12-07 00:09:01 -0200

    testing f1 score ideas

--- network.py
+++ network.py
@@ -2,10 +2,11 @@
 packages
 '''
 import keras
-from keras.models import Sequential
-from keras.layers import Dense
 import numpy as np
 import random
+from keras.models import Sequential
+from keras.layers import Dense
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
@@ -80,6 +81,29 @@ model.fit(X,Y, epochs=20, batch_size=5)
 '''
 5. Evaluate model
 '''
+
+import numpy as np
+from keras.callbacks import Callback
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+class Metrics(Callback):
+def on_train_begin(self, logs={}):
+ self.val_f1s = []
+ self.val_recalls = []
+ self.val_precisions = []
+
+def on_epoch_end(self, epoch, logs={}):
+ val_predict = (np.asarray(self.model.predict(self.model.validation_data[0]))).round()
+ val_targ = self.model.validation_data[1]
+ _val_f1 = f1_score(val_targ, val_predict)
+ _val_recall = recall_score(val_targ, val_predict)
+ _val_precision = precision_score(val_targ, val_predict)
+ self.val_f1s.append(_val_f1)
+ self.val_recalls.append(_val_recall)
+ self.val_precisions.append(_val_precision)
+ print “ — val_f1: %f — val_precision: %f — val_recall %f” %(_val_f1, _val_precision, _val_recall)
+ return
+
+metrics = Metrics()
 #F1_score
 #https://en.wikipedia.org/wiki/F1_score
 #threshold on numpy
@@ -88,3 +112,10 @@ model.fit(X,Y, epochs=20, batch_size=5)
 # a = np.array([[0.2, 0.3, 0.5, 0.7, 0.9, 0.8],
 #               [0.2, 0.4, 0.5, 0.9, 0.3, 0.1]])
 # np.where(a >= 0.5, 1, 0)
+
+'''
+threshold
+'''
+a = np.array([[0.2, 0.3, 0.5, 0.7, 0.9, 0.8],
+              [0.2, 0.4, 0.5, 0.9, 0.3, 0.1]])
+np.where(a >= 0.5, 1, 0)
