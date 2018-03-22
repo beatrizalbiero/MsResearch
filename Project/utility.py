afac94a38170c6fed771f34d2bf5a4a9473a69ff
Beatriz Albiero <beatrizalbiero1@gmail.com>
2018-03-22 10:54:36 -0300

    added utility (load_data)

---
+++ Project/utility.py
@@ -0,0 +1,33 @@
+"""
+Utilities
+"""
+def load_data(path):
+    """
+    Create a pandas data frame with columns: 'infinitive':
+    the verb's infinitive, 'vec_inf': the coded infinitive form,
+    'f person': the verb's first person conjugation (in portuguese),
+    'vec_I': the coded conjugate form.
+
+    :path type: string
+    :r type: tuple
+    """
+    import pandas as pd
+    with open(path,'r') as csvfile:
+        readcsv = csv.reader(csvfile, delimiter = ',')
+        phoneticinf = []
+        phoneticI = []
+        for row in readcsv:
+            phoneticinf.append(row[1])
+            phoneticI.append(row[3])
+        cf.dataTest(phoneticinf,phoneticI) #tests if dataset is ok
+        vec_inf = []
+        vec_I = []
+        for item in phoneticinf:
+            vec_inf.append(cf.coding(item))
+        for item in phoneticI:
+            vec_I.append(cf.coding(item))
+    d = {'infinitive': phoneticinf, 'vec_inf': vec_inf, 'f person': phoneticI, 'vec_I': vec_I}
+    df = pd.DataFrame(data=d)
+    X = np.array(df['vec_inf'].tolist())
+    Y = np.array(df['vec_I'].tolist())
+    return X,Y
