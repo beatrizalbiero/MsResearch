d2a4d12ab19e2c6644483b9ca243a3f8f285385b
Beatriz Albiero <beatrizalbiero1@gmail.com>
2018-04-08 09:57:45 -0300

    added the option of verifying the lenght of unique verbs

--- Project/utility.py
+++ Project/utility.py
@@ -4,10 +4,9 @@ import pandas as pd
 import csv
 import coding_function as cf
 import numpy as np
-from pathlib import Path
 
 
-def load_data(path):
+def load_data(path, len_set=False):
     """
     Create a pandas data frame.
 
@@ -16,7 +15,10 @@ def load_data(path):
     'f person': the verb's first person conjugation (in portuguese),
     'vec_I': the coded conjugate form.
 
+    If len_set is True, returns the lenght of the unique values of the dataset.
+
     :path type: string
+    :len_set type: bool
     :r type: tuple
     """
     with open(path, 'r') as csvfile:
@@ -33,9 +35,13 @@ def load_data(path):
             vec_inf.append(cf.coding(item))
         for item in phoneticI:
             vec_I.append(cf.coding(item))
-    d = {'infinitive': phoneticinf, 'vec_inf': vec_inf, 'f person': phoneticI,
-         'vec_I': vec_I}
-    df = pd.DataFrame(data=d)
-    X = np.array(df['vec_inf'].tolist())
-    Y = np.array(df['vec_I'].tolist())
-    return X, Y
+        d = {'infinitive': phoneticinf, 'vec_inf': vec_inf,
+             'f person': phoneticI, 'vec_I': vec_I}
+        df = pd.DataFrame(data=d)
+        X = np.array(df['vec_inf'].tolist())
+        Y = np.array(df['vec_I'].tolist())
+        if len_set is False:
+            return X, Y
+        else:
+            print(len(set(phoneticinf)))
+            return X, Y
